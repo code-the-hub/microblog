840dd747ad3f42f22dc04e57a7f840fcfcf90ebd
Kamal Koranga <kamalkoranga75@gmail.com>
2023-09-18 20:14:16 +0530

    mail_use_ssl

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['kamalkoranga75@gmail.com']
